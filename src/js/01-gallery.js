2a39bb8c6bb236cb028ba5335626c3290da5eb92
adoppp <121878757+adoppp@users.noreply.github.com>
2023-06-09 18:43:37 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,49 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
+
 
 console.log(galleryItems);
 
-const galleryList = document.querySelector('.gallery');
+const container = document.querySelector('.gallery');
+const markupGallery = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+       
+   </a>
+</li>`);
 
-// Создание и рендеринг разметки
-const createGalleryItem = ({ preview, original, description }) => {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery__item');
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = original;
-    const image = document.createElement('img');
-    image.classList.add('gallery__image');
-    image.src = preview;
-    image.setAttribute('data-source', original);
-    image.alt = description;
-    link.appendChild(image);
-    galleryItem.appendChild(link);
-    return galleryItem;
-  };
-  const galleryMarkup = galleryItems.map(createGalleryItem);
-  galleryList.append(...galleryMarkup);
-  // Реализация делегирования и получение URL большого изображения
-  galleryList.addEventListener('click', handleGalleryItemClick);
-  function handleGalleryItemClick(event) {
-    event.preventDefault();
-    const target = event.target;
-    if (target.nodeName !== 'IMG') {
-      return;
-    }
-    const source = target.dataset.source;
-    openModal(source);
-  }
-  // Открытие модального окна по клику на элементе галереи
-  function openModal(source) {
-    const instance = basicLightbox.create(`<img src="${source}" width="800" height="600">`);
-    instance.show();
-    // Закрытие модального окна при нажатии на кнопку Esc
-    const handleKeyPress = (event) => {
-      if (event.code === 'Escape') {
-        instance.close();
-      }
-    };
-    window.addEventListener('keydown', handleKeyPress);
-  }
+container.insertAdjacentHTML('beforeend', markupGallery.join(""));
+
+
+const galleryList = new SimpleLightbox('.gallery a',{
+   captionsData: 'alt',   
+   captionDelay: 250,
+});
